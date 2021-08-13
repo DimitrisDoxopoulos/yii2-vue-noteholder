dd8d4a0c17c5ef55842790a41e67aaac21de714d
Αδαμ Κωνσταντινος <adamkwn93@gmail.com>
2021-08-13 17:51:31 +0300

    adding files

---
+++ vuejs/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <notes />
+  </div>
+</template>
+
+<script>
+import Notes from "./components/Notes";
+
+export default {
+  name: 'App',
+  components: {Notes},
+}
+</script>
+
+<style>
+
+</style>
